8c0ee3138b2b210abbb4f953ea6226422a0b1714
youngsoon-work <youngsoon.work@gmail.com>
2026-06-29 22:10:30 +1200

    0629 design update

---
+++ project/fig_endpoint/CaseStudy5Endpoint.d.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+export interface CaseStudy5EndpointProps {
+  className?: string;
+  style?: React.CSSProperties;
+}
+export declare const CaseStudy5Endpoint: React.FC<CaseStudy5EndpointProps>;
+export default CaseStudy5Endpoint;
